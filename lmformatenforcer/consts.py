d083ccefde694f5f257419c74d9e38695e5114ec
isamu-isozaki <isamu.website@gmail.com>
2024-02-18 00:33:05 -0500

    Remove whitespace

--- lmformatenforcer/consts.py
+++ lmformatenforcer/consts.py
@@ -1,5 +1,5 @@
 COMPLETE_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{};:,./<>? `'\""
-MAX_CONSECUTIVE_WHITESPACES = 12
+MAX_CONSECUTIVE_WHITESPACES = 0
 WHITESPACE_CHARACTERS = " \t\n\r"
 BACKSLASH = "\\"
 BACKSLASH_ESCAPING_CHARACTERS = '"\\/bfnrt'  # Characters allowed after an escaping backslash, except unicode
